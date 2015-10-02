1cc34554410955fc93099aa174c3f102b0a8b8bc
Jacobchar <jj.charlebois@gmail.com>
2015-10-01 18:52:51 -0600

    Still working on multiplayer modes; Wondering if it is possible to correct previous commit typo's ...

--- app/src/main/java/com/example/jacob/assignment1reactiontimer/Multiplayer.java
+++ app/src/main/java/com/example/jacob/assignment1reactiontimer/Multiplayer.java
@@ -16,7 +16,7 @@ public class Multiplayer extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.single_player);
+        setContentView(R.layout.multiplayer);
     }
 
     @Override
--- app/src/main/java/com/example/jacob/assignment1reactiontimer/TwoPlayer.java
+++ app/src/main/java/com/example/jacob/assignment1reactiontimer/TwoPlayer.java
@@ -1,7 +1,35 @@
 package com.example.jacob.assignment1reactiontimer;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
 /**
  * Created by Jacob on 2015-10-01.
  */
-public class TwoPlayer {
+public class TwoPlayer extends AppCompatActivity {
+
+    /*The following two methods are necessary to create the screen and handle the options menu*/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.two_player);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
 }
