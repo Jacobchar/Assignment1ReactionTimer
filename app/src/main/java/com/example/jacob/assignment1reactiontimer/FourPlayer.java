665566972ed280fb24715960bc8a7f734b768700
Jacobchar <jj.charlebois@gmail.com>
2015-10-02 16:41:31 -0600

    added licensing

--- app/src/main/java/com/example/jacob/assignment1reactiontimer/FourPlayer.java
+++ app/src/main/java/com/example/jacob/assignment1reactiontimer/FourPlayer.java
@@ -1,12 +1,26 @@
+/*
+    Reaction Timer: Reaction timer and multi-player game show buzzer
+    Copyright (C) 2015  Jacob Charlebois
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.example.jacob.assignment1reactiontimer;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
 
 /**
